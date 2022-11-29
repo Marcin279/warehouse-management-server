ae4a1535e897dad93783ce14c79ddc46f7a6e31f
Marcin279 <marcin.biela12@gmail.com>
2022-11-29 19:16:03 +0100

    Update app models

--- wms_api/models.py
+++ wms_api/models.py
@@ -3,9 +3,6 @@ from django.db import models
 
 # Create your models here.
 
-class Category(models.Model):
-    name = models.CharField(max_length=30)
-
 
 class Product(models.Model):
     Product1 = 'P1'
@@ -30,7 +27,7 @@ class Product(models.Model):
     category = models.CharField(max_length=30, default='Phones')
 
     def __str__(self):
-        return self.product_type
+        return self.product_name
 
 
 class Package(models.Model):
@@ -67,56 +64,35 @@ class ProductStore(models.Model):
     quantity = models.PositiveIntegerField(blank=True, null=True)
 
 
-class Suppliers(models.Model):
-    name = models.CharField(max_length=30)
-
-
-class AddressDetails(models.Model):
+class ShipmentDetails(models.Model):
     buildings_number = models.IntegerField(blank=True, null=True)
     street = models.CharField(max_length=30)
     postal_code = models.CharField(max_length=30)
     city = models.CharField(max_length=30)
     country = models.CharField(max_length=30)
+    total_weight = models.IntegerField(blank=True, null=True)
 
 
-class User(models.Model):
+class Worker(models.Model):
     login = models.CharField(max_length=30)
     password = models.CharField(max_length=30)
     name = models.CharField(max_length=30)
     surname = models.CharField(max_length=30)
     role = models.CharField(max_length=30)
-    address_details = models.ManyToManyField(AddressDetails)
+    address_details = models.ManyToManyField(ShipmentDetails)
 
 
 class Warehouse(models.Model):
-    address_details = models.ForeignKey(AddressDetails, on_delete=models.CASCADE)
-    id_suppliers = models.ForeignKey(Suppliers, on_delete=models.CASCADE)
+    address_details = models.ForeignKey(ShipmentDetails, on_delete=models.CASCADE)
 
 
-class WarehouseSummary(models.Model):
-    warehouse = models.ForeignKey(Warehouse, on_delete=models.CASCADE)
-
-
-class ProductStock(models.Model):
+class WarehouseStock(models.Model):
     quantity = models.IntegerField()
     warehouse = models.ForeignKey(Warehouse, on_delete=models.CASCADE)
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
 
 
-class ProductOrder(models.Model):
-    quantity = models.IntegerField()
-    order = models.IntegerField()
-    product = models.IntegerField()
-
-
-class Order(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    user_address_details = models.ForeignKey(AddressDetails, on_delete=models.CASCADE)
-    shipment_type = models.CharField(max_length=30)
-    creation_date = models.DateTimeField()
-    order_col = models.CharField(max_length=30)
-
-
+# ============================EXAMPLES===========================================================
 class Modules(models.Model):
     module_name = models.CharField(max_length=50)
     module_duaration = models.IntegerField()
